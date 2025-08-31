0a8dc9a8b7ec2651cf81d9730961e1809cf64f4f
becomingraghav <sphinxrelam@gmail.com>
2025-08-31 22:18:51 +0530

    Update day3(b).c

--- day3(b).c
+++ day3(b).c
@@ -1,16 +1,16 @@
-///Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+///Q6: Write a program to swap two numbers using a third variable.
 
 /*
 Sample Test Cases:
 Input 1:
-0
+3 5
 Output 1:
-Fahrenheit=32
+After swap: 5 3
 
 Input 2:
-100
+-1 1
 Output 2:
-Fahrenheit=212
+After swap: 1 -1
 
 */
 
