49dbaa290f6a22503f53865e40219498132e1e23
becomingraghav <sphinxrelam@gmail.com>
2025-08-31 22:11:26 +0530

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,14 +1,35 @@
+
+
+
 #include <stdio.h>
 int main(){
+
     int x;
-    printf("Enter First Number:");
+    printf("Enter first Number :");
     scanf("%d",&x);
     int y;
-     printf("Enter Second Number:");
+    printf("Enter Second Number :");
     scanf("%d",&y);
+
     int sum = x+y;
-    printf("The sum is: %d", sum);
+    int diff= x-y;
+    int prod=x*y;
+    
+     printf("Sum:%d\n",sum);
+     printf("Diff:%d\n",diff);
+     printf("Product:%d\n",prod);
+    int Quot;
+      
 
-    return 0;
+     if (y==0)
+     {
+         printf("Quotient: NOT DEFINED(cannot divide by 0)\n");
+     }
+     else{
+        Quot=x / y;
+        printf("Quotient:%d\n",Quot);
+     }
+     
 
+ return 0;
 }
