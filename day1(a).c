8cf125938d706fac32dc792beeb321c728e401d2
becomingraghav <sphinxrelam@gmail.com>
2025-10-14 22:36:00 +0530

    Add files via upload

---
+++ day1(a).c
@@ -0,0 +1,46 @@
+<<<<<<< HEAD:day1.c
+////Q1: Write a program to input two numbers and display their sum.
+=======
+///Q1: Write a program to input two numbers and display their sum.
+>>>>>>> 2a286e62ad44ce43917ef5a3e5f8bcb72ec0a5f6:day1(a).c
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+
+<<<<<<< HEAD:day1.c
+
+
+
+
+
+
+
+
+=======
+>>>>>>> 2a286e62ad44ce43917ef5a3e5f8bcb72ec0a5f6:day1(a).c
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter First Number:");
+    scanf("%d",&x);
+    int y;
+     printf("Enter Second Number:");
+    scanf("%d",&y);
+    int sum = x+y;
+    printf("The sum is: %d", sum);
+
+    return 0;
+
+}
